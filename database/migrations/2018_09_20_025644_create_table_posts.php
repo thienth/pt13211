d7b29a67660b63cc0ba1fdfbc37fb7bdbf295553
thienth32 <thienth32@gmail.com>
2018-09-22 10:37:10 +0700

    update

---
+++ database/migrations/2018_09_20_025644_create_table_posts.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTablePosts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title')->unique();
+            $table->text('short_desc')->nullable();
+            $table->text('content')->nullable();
+            $table->string('image')->nullable();
+            $table->datetime('publish_date')->nullable();
+            $table->integer('status')->default(1);
+            $table->string('author')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
